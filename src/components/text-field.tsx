77d24cf7fe1caa1b30d56ef892f6f26d8aea44b6
IAG Work Sample <iag-work-sample@example.com>
2020-08-02 23:26:19 +1000

    Made the material textboxes look ok.

--- src/components/text-field.tsx
+++ src/components/text-field.tsx
@@ -1,18 +1,95 @@
 import React from "react";
 
-type TextProps = {
+import styled from "styled-components";
+
+type CommonProps = {
+  placeholder?: string;
+  className?: string;
+};
+
+type TextProps = CommonProps & {
   value?: string | null;
   onChange: (newValue: string | null) => void;
   type: "text" | "date";
 };
 
-type NumberProps = {
+type NumberProps = CommonProps & {
   value?: number | null;
   onChange: (newValue: number | null) => void;
   type: "number";
 };
 
 type Props = TextProps | NumberProps;
+
+const PRIMARY_COLOR = "#6200ee;";
+
+const Root = styled.div`
+  // display: inline-block;
+  background: #f8f8f8;
+  // padding: 1rem;
+  position: relative;
+
+  border-bottom: 1px solid #ccc;
+
+  &:focus-within {
+    // border-bottom: none;
+    background: #dddddd;
+  }
+
+  transition: all 200ms ease;
+`;
+
+const Input = styled.input`
+  border: none;
+  background: none;
+  height: 100%;
+  width: 100%;
+  margin: 1.5rem 1rem 0.75rem 1rem;
+`;
+
+const Ripple = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-bottom: 2px solid ${PRIMARY_COLOR};
+  scale: 0;
+
+  ${Root}:focus-within & {
+    scale: 1;
+  }
+
+  transition: all 200ms ease;
+`;
+
+interface PlaceholderProps {
+  inputValue?: string;
+}
+
+const selectedPlaceholderCss = `
+  scale: 0.5;
+  transform: translateY(-100%);
+`;
+
+const Placeholder = styled.label<PlaceholderProps>`
+  position: absolute;
+  left: 1rem;
+  top: 1.25rem;
+  font-size: 1.1rem;
+  color: #555;
+  transform-origin: top left;
+  pointer-events: none;
+
+  ${(props) =>
+    props.inputValue && props.inputValue !== "" && selectedPlaceholderCss}
+
+  ${Root}:focus-within & {
+    ${selectedPlaceholderCss}
+    color: ${PRIMARY_COLOR}
+  }
+
+  transition: all 200ms ease;
+`;
 
 export default function TextField(props: Props) {
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,10 +104,14 @@ export default function TextField(props: Props) {
   };
 
   return (
-    <input
-      type={props.type || "text"}
-      value={props.value || ""}
-      onChange={onChange}
-    />
+    <Root className={props.className}>
+      <Input type="text" value={props.value || ""} onChange={onChange} />
+      {props.placeholder && (
+        <Placeholder inputValue={props.value?.toString()}>
+          {props.placeholder}
+        </Placeholder>
+      )}
+      <Ripple />
+    </Root>
   );
 }
--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 import { useAsyncCallback, useAsync } from "react-async-hook";
+import moment from "moment";
+import styled from "styled-components";
 
 import TextField from "../components/text-field";
 import createEmployee from "../api/create-employee";
-import moment from "moment";
+
+const FormElement = styled.form`
+  // display: flex;
+  // flex-direction: column;
+  padding: 0.5rem;
+  max-width: 30rem;
+  margin: 0 auto;
+`;
+
+const StyledTextField = styled(TextField)`
+  margin: 0.5rem 0;
+`;
+
+const SubmitButton = styled.button`
+  margin: 0.25rem 0;
+`;
 
 export default function Form() {
-  const [name, setName] = useState<string | null>("name");
-  const [salary, setSalary] = useState<number | null>(123);
-  const [dob, setDob] = useState<string | null>("2020/02/02");
+  const [name, setName] = useState<string | null>(null);
+  const [salary, setSalary] = useState<number | null>(null);
+  const [dob, setDob] = useState<string | null>(null);
 
   const create = async () => {
     if (!name || !salary || !dob) {
@@ -27,13 +44,29 @@ export default function Form() {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <FormElement onSubmit={onSubmit}>
       {asyncCreate.loading && "Loading..."}
       {asyncCreate.error && asyncCreate.error.message}
-      <TextField value={name} onChange={setName} type="text" />
-      <TextField value={salary} type="number" onChange={setSalary} />
-      <TextField value={dob} onChange={setDob} type="date" />
-      <button type="submit">Submit</button>
-    </form>
+      IAG Form
+      <StyledTextField
+        value={name}
+        onChange={setName}
+        type="text"
+        placeholder="Name"
+      />
+      <StyledTextField
+        value={salary}
+        type="number"
+        onChange={setSalary}
+        placeholder="Salary"
+      />
+      <StyledTextField
+        value={dob}
+        onChange={setDob}
+        type="date"
+        placeholder="Date of Birth"
+      />
+      <SubmitButton type="submit">Submit</SubmitButton>
+    </FormElement>
   );
 }
